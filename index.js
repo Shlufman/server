6ac2a2627f440d517ed635cd2f2867f7cfa6aaa9
Shlyufman Konstantin <shlufman@mail.ru>
2024-02-02 14:26:47 +0200

    attempt fix 19

--- index.js
+++ index.js
@@ -24,28 +24,32 @@ const app = express();
 //         next();
 //     }
 // });
-const corsOptions = {
-    origin: /\.onrender\.com$/,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-withCredentials: true};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: /\.onrender\.com$/,
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// withCredentials: true};
+// app.use(cors(corsOptions));
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     res.header('Access-Control-Allow-Credentials', true);
-//
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Origin', '*');
-//         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//         res.header('Access-Control-Allow-Headers', 'Content-Type');
-//         res.header('Access-Control-Allow-Credentials', true);
-//         res.sendStatus(200);
-//     } else {
-//         next();
-//     }
-// });
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+
+    next();
+});
 
 app.options("/simple-cors", (req, res) => {
     console.log('!!!')
