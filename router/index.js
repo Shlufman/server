421ed5a82dd302bc4814241e512487b4d2fbe8bf
Shlyufman Konstantin <shlufman@mail.ru>
2024-02-02 07:46:32 +0200

    attempt fix cors 7

--- router/index.js
+++ router/index.js
@@ -1,41 +1,29 @@
-const { Router } = require('express');
-const router = Router();
+const express = require('express');
 const cors = require('cors');
 const { body } = require('express-validator');
 const authMiddleware = require('../middlewares/auth-middleware');
 const userController = require('../controllers/user-controller');
 
+const app = express();
+
 // Используем cors middleware для обработки CORS-заголовков
-router.use(cors({
-  origin: '*', // Укажите конкретный источник вашего фронтенда
+app.use(cors({
+  origin: '*', // Разрешаем доступ с любого источника
   methods: 'GET, POST, PUT, DELETE', // Укажите нужные методы
   allowedHeaders: 'Content-Type', // Укажите разрешенные заголовки
   credentials: true, // Разрешаем отправку куки и заголовка Authorization
 }));
 
-// router.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*'); // или указать конкретные источники
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Укажите нужные методы
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//
-//     if (req.method === 'OPTIONS') {
-//         res.sendStatus(200); // Отправляем успешный ответ для предварительного запроса
-//     } else {
-//         next();
-//     }
-// });
-
-router.post('/registration',
+app.post('/registration',
     body('email').isEmail(),
     body('password').isLength({min: 3, max: 32}),
     userController.registration
 );
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-router.get('/activate/:link', userController.activate);
-router.get('/refresh', userController.refresh);
-router.get('/users', authMiddleware, userController.getUsers);
 
-module.exports = router
+app.post('/login', userController.login);
+app.post('/logout', userController.logout);
+app.get('/activate/:link', userController.activate);
+app.get('/refresh', userController.refresh);
+app.get('/users', authMiddleware, userController.getUsers);
+
+module.exports = app
