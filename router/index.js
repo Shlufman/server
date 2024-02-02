2a832def2f1842d040eae6ace68dc286ac37d13d
Shlyufman Konstantin <shlufman@mail.ru>
2024-02-02 08:09:12 +0200

    attempt fix cors 10

--- router/index.js
+++ router/index.js
@@ -14,7 +14,7 @@ app.use(cors({
   credentials: true, // Разрешаем отправку куки и заголовка Authorization
 }));
 
-app.options('*', cors());
+app.options('/registration', cors());
 
 app.post('/registration',
     cors({ origin: ['*', 'http://localhost:5500', 'http://localhost:3000' ]}),
