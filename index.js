df662885c81ac2334debd200497b5ba7466d705e
Shlyufman Konstantin <shlufman@mail.ru>
2024-02-02 10:21:25 +0200

    attempt fix 11

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ app.use((req, res, next) => {
     }
 });
 
+app.options('*', (req, res) => {
+    console.log('!!!')
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.sendStatus(200);
+});
+
 // Пример маршрута
 app.get("/simple-cors", (req, res) => {
     console.info("GET /simple-cors");
