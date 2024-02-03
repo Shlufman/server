8c4c2ad624d25a263e554f1456ea6ebcf6fa0fda
Shlyufman Konstantin <shlufman@mail.ru>
2024-02-03 17:40:24 +0200

    attempt fix 21

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use((req, res, next) => {
 
 app.options("/simple-cors", (req, res) => {
     console.log('!!!')
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', true);
