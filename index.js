3cce5ddef18214f58cfbbc53675c71e148ceb673
Shlyufman Konstantin <shlufman@mail.ru>
2024-02-03 18:29:29 +0200

    attempt fix 25

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const app = express();
 app.use((req, res, next) => {
     res.setHeader(
         "Access-Control-Allow-Origin",
-        "*"
+        'http://localhost:3000'
     );
     res.setHeader(
         "Access-Control-Allow-Methods",
